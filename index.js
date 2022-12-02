58d69726ad83cd7cf6056550571f044fa11a7817
Walid Berrouk <jw_berrouk@esi.dz>
2022-12-02 14:38:03 +0100

    Initial commit with necessary packages

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const cors = require('cors')
+const { addProfile, getProfiles } = require("./controllers/profiles")
+const { getUsers, addUser, getUser, modifyUser, deleteUser } = require("./controllers/users")
+const login = require("./controllers/login")
+const verifyToken = require("./middlewares/verifyToken")
+const jwt = require('jsonwebtoken');
+const morgan = require("morgan")
+
+// Init the app
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(morgan())
+
+const secretKey = 'youCantCatchme098765%^&*##@'
+
+// File serving
+app.use('/static', express.static('public'))
+app.use('/private', verifyToken, (req, res, next) => {
+    jwt.verify(req.token, secretKey, (err, authData) => {
+        if (err) {
+            res.sendStatus(403)
+        } else {
+            next()
+        }
+    })
+}, express.static('private'))
+
+// Create Route handlers
+
+// Profiles
+app.post('/profile', addProfile)
+app.get('/profiles', verifyToken, getProfiles)
+
+// Users
+app.get('/users', getUsers)
+app.post('/user', addUser)
+app.get('/user/:id', getUser)
+app.put('/user/:id', modifyUser)
+app.delete('/user/:id', deleteUser)
+
+// Login
+app.post('/api/login', login)
+
+
+// Listen on a port
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
